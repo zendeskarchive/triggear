c0dc0c644fd8912d58deb416955e85259d22618e
karolgil <karol.gil@getbase.com>
2017-11-24 15:48:14 +0100

    Fix test in request parsing

--- tests/github_controller/test_request_parsing.py
+++ tests/github_controller/test_request_parsing.py
@@ -6,7 +6,7 @@ pytestmark = pytest.mark.asyncio
 
 
 async def test_get_req_json(gh_sut: GithubController, mock_request):
-    assert await gh_sut.get_request_json(mock_request) == 'json'
+    assert await gh_sut.get_request_json(mock_request) == {'json': 'json'}
 
 
 async def test_get_req_event_header(gh_sut: GithubController, mock_request):
