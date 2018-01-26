79cbfc35ecca75434cf31839416e5866bad7909d
karolgil <karol.gil@getbase.com>
2018-01-26 13:35:19 +0100

    Add logging to file on side of logging to stdout

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 import logging
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+logging.root.handlers = []
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    filename='triggear.log')
+
+console = logging.StreamHandler()
+console.setLevel(logging.WARNING)
+logging.getLogger("").addHandler(console)
