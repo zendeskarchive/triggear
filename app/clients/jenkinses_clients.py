fef3a8c042bd310d7048c07eecd947e77e24112c
karolgil <karol.gil@getbase.com>
2018-02-08 17:11:45 +0100

    WIP

--- app/clients/jenkinses_clients.py
+++ app/clients/jenkinses_clients.py
@@ -17,20 +17,15 @@ class JenkinsesClients:
         client = self.__jenkins_clients.get(url)
         if client is None:
             logging.warning(f'Missing client for {url} - will create one')
-            self.setup_jenkins_client(url)
+            self.__setup_jenkins_client(url)
         return self.__jenkins_clients[url]
 
-    def setup_jenkins_client(self, url: str) -> None:
+    def __setup_jenkins_client(self, url: str) -> None:
         if self.config.jenkins_instances.get(url) is None:
             logging.warning(f'Missing Jenkins {url} in current config. Will reload the config to check for new instances')
-            try:
-                new_config = TriggearConfig()
-                if new_config.jenkins_instances.get(url) is None:
-                    raise TriggearError(f'Jenkins {url} not defined in current config. Please add its definition to creds file')
-                else:
-                    self.config = new_config
-            except (yaml.YAMLError, KeyError):
-                logging.exception('Exception caught when trying to reconfigure for new Jenkins instance in GithubController.'
-                                  'Please check validity of provided config. Sticking with old one for now.')
-                raise
+            new_config = TriggearConfig()
+            if new_config.jenkins_instances.get(url) is None:
+                raise TriggearError(f'Jenkins {url} not defined in current config. Please add its definition to creds file')
+            else:
+                self.config = new_config
         self.__jenkins_clients[url] = JenkinsClient(self.config.jenkins_instances[url])
---
+++ tests/clients/test_jenkinses_clients.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+from mockito import mock, when
+
+from app.clients.jenkins_client import JenkinsClient
+from app.clients.jenkinses_clients import JenkinsesClients
+from app.config.triggear_config import TriggearConfig
+from app.exceptions.triggear_error import TriggearError
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestJenkinsesClients:
+    async def test__when_jenkins_for_url_is_not_in_config__should_raise(self):
+        triggear_config: TriggearConfig = mock({'jenkins_instances': {}}, spec=TriggearConfig, strict=True)
+        when(os).getenv('CREDS_PATH', 'creds.yml').thenReturn('./tests/config/example_configs/creds.yaml')
+        jenkinses_clients = JenkinsesClients(triggear_config)
+
+        with pytest.raises(TriggearError):
+            jenkinses_clients.get_jenkins('url')
+
+    async def test__when_jenkins_for_url_is_in_new_config__should_return_it(self):
+        triggear_config: TriggearConfig = mock({'jenkins_instances': {}}, spec=TriggearConfig, strict=True)
+        when(os).getenv('CREDS_PATH', 'creds.yml').thenReturn('./tests/config/example_configs/creds.yaml')
+        jenkinses_clients = JenkinsesClients(triggear_config)
+
+        jenkins_client: JenkinsClient = jenkinses_clients.get_jenkins('https://ci.triggear.com/')
+        assert jenkins_client.config.url == 'https://ci.triggear.com/'
+        assert jenkins_client.config.token == "other_api_token"
+        assert jenkins_client.config.username == "other_user"
