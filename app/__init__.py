c866f52d9c6c76bbe87c16ffe59f9e039df3b7fe
karolgil <karol.gil@getbase.com>
2018-02-12 09:31:03 +0100

    Add proper formatter to stdout logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import logging
 
 logging.root.handlers = []
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+fmt = '%(asctime)s %(levelname)-8s %(message)s'
+logging.basicConfig(format=fmt,
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     filename='triggear.log')
 
 console = logging.StreamHandler()
 console.setLevel(logging.WARNING)
+console.setFormatter(logging.Formatter(fmt))
 logging.getLogger("").addHandler(console)
